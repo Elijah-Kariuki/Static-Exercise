5a88ca72b35a9988434b20ecd9f206655c3de265
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-10-20 12:41:40 -0400

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace StaticExercise
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"65 degrees Fahrenheit converts to {TempConverter.FahrenheitToCelsius(65)} degrees Celsius");
+            Console.WriteLine($"18 degrees Celsius converts to {TempConverter.CelsiusToFahrenheit(18)} degrees Fahrenheit");
         }
     }
 }
